cf38c9f079673c8b48bb79e3e99a5be35a63a4fa
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-07 08:06:49 +0200

    add and register the resource in this module

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@
 import List from './List';
 import Update from './Update';
 import './templates';
+import Resource from './Resource';
 
 export default angular.module('monad.crud', ['monad.cms', List, Update, 'monad.crud.templates'])
+    .factory('monadResource', Resource)
+    .factory('moResource', Resource)
     .name;
 
