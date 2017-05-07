85af30b8867e00798d944d5d3e224b6ef2f34efa
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-07 08:09:03 +0200

    also add the field directive here

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import List from './List';
 import Update from './Update';
 import './templates';
 import Resource from './Resource';
+import Field from './Field';
 
 export default angular.module('monad.crud', ['monad.cms', List, Update, 'monad.crud.templates'])
     .factory('monadResource', Resource)
     .factory('moResource', Resource)
+    .directive('monadField', Field)
+    .directive('moField', Field)
     .name;
 
