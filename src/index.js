b33156bfbe327aa2f3d78bc04daf178ea0bde153
Marijn Ophorst <marijn@sensimedia.nl>
2023-06-01 14:18:20 +0200

    this was a silly directive

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import List from './List';
 import Update from './Update';
 import '../lib/templates';
 import Resource from './Resource';
-import Field from './Field';
 import Delete from './Delete';
 
 export default angular.module('monad.crud', ['ngResource', 'monad.cms', List, Update, 'monad.crud.templates'])
     .factory('monadResource', Resource)
-    .directive('monadField', Field)
     .service('monadDelete', Delete)
     .name;
 
